39ba138290e08d3d2fddf61be8b58d9568d0a831
Antón Zuev <anton.zuev@autentia.com>
2022-01-12 11:50:02 +0100

    Initial Commit

---
+++ Shared/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  Shared
+//
+//  Created by Antón Zuev on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
